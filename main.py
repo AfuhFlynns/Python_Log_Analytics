af852d3dc854ce799e2526d6c669e48857c8dd1d
AfuhFlynns <flyinnsafuh@gmail.com>
2025-01-18 21:59:02 +0100

    Complete project to be continued

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 # Import utils data
-from "utils.py" import Log_Handler
+from utils import Log_Handler
 
 class App:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = "John Dalton"
+        self.successful = False
 
     def get_user_name(self):
-        user_name = input("Enter your name here: ")
+        user_name = input("Please enter your name here: ")
         return user_name
 
     def intro(self):
         print("**** This is simple log analytics program ****")
 
     def call_utils_methods(self):
-        log_handler = Log_Handler()
+        log_handler = Log_Handler(self.name)
+
+        if self.name == "":
+            self.name = "User"
+
+        print(f"\n******** Hello, {self.name} do the following ********")
+        self.successful = log_handler.handle_value_error()
+
+        if self.successful == True:
+            self.successful = log_handler.handle_division_by_zero_error ()
+
+        if self.successful == True:
+            self.successful = log_handler.handle_file_write_read_error()
 
     def app(self):
         self.intro()
         self.name = self.get_user_name()
         self.call_utils_methods()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.app()
--- utils.py
+++ utils.py
@@ -5,13 +5,17 @@ import os.path
 
 
 class Log_Handler:
-    def handle_log_file(self, level, message, stack_trace):
+    def __init__(self, name):
+        self.name = name
+
+
+    def handle_log_file(self, level, message, stack_trace, user_name):
         # File path setup
         log_path = './logfile.log'
         # Open file in append mode
         f = open(log_path, 'a')
         # Modify file content
-        f.write(f'Time: {datetime.datetime.now()}-Level: {level}-Message: {message}-StackTrace: {stack_trace}\n')
+        f.write(f'Time: {datetime.datetime.now()},  Level: {level},  Message: {message},  StackTrace: {stack_trace}, Author: {user_name}\n')
         # Close every file after opening
         f.close()
 
@@ -22,14 +26,80 @@ class Log_Handler:
 
         # f.close()
 
-
+    def print_user_input(self, value):
+        print(f"You entered: {value}")
 
     def handle_value_error(self):
         try:
-            value = int(input("If you would please enter an integer value: "))
-            print(f"You entered: {value}")
-        except ValueError as error:
-            self.handle_log_file("ERROR", "Expected an integer as input but a non-integer value was provided")
+            # Handle integer input
+            num = int(input("Please enter an integer value: "))
+            self.print_user_input(num)
+            # Handle float input
+            height = float(input("Enter a float "))
+            self.print_user_input(height)
+        except ValueError as e:
+            # Handle exception and log it to the log file
+            self.handle_log_file("ERROR", "Input type not allowed!", e, self.name)
+            print(f"Sorry you entered an unauthorized value!")
+        except KeyboardInterrupt as e:
+            self.handle_log_file("WARN", "Keyboard interrupted script execution", e, self.name)
+            print(f"Sorry program was interrupted!")
         finally:
-            print("Well done, you have successfully entered an integer")
-            print(value)
+            return True
+
+    def handle_division_by_zero_error(self):
+        try:
+            num1 = 4
+            num2 = int(input("Enter a value greater than zero to divide by (4): "))
+            result = num1 / num2
+        except ValueError as e:
+            # Handle exception and log it to the log file
+            self.handle_log_file("ERROR", "Input type not allowed!", e, self.name)
+            print(f"Sorry you entered an unauthorized value!")
+        except ZeroDivisionError as e:
+            self.handle_log_file("ERROR", "User entered unauthorized value", e, self.name)
+            print(f"Sorry you entered an unauthorized value!")
+        except KeyboardInterrupt as e:
+            self.handle_log_file("WARN", "Keyboard interrupted script execution", e, self.name)
+            print(f"Sorry program was interrupted!")
+        finally:
+            return True
+
+    def handle_file_write_read_error(self):
+        try:
+            file_path = str(input("Enter a file path for a file to be read and displayed: "))
+            # Open the file in read mode
+            read_file = open(f"./{file_path}", "r")
+            count = 0
+            for line in read_file:
+                count = count + 1
+                print(f"Line: {count}, {line}\n")
+            print("File read complete!!")
+
+            read_file.close()
+
+            file_path = str(input("Enter a file path for a file to write into: "))
+            # Open the file in write mode
+            write_file = open(f"./{file_path}", "w")
+            count = 0
+            while count < 3:
+                write_file.write(f"Line: {count}, Some text here\n")
+                count = count + 1
+            print("File write complete!!")
+
+            write_file.close()
+        except ValueError as e:
+            # Handle exception and log it to the log file
+            self.handle_log_file("ERROR", "Input type not allowed!", e, self.name)
+            print(f"Sorry you entered an unauthorized value!")
+        except FileExistsError as e:
+            self.handle_log_file("INFO", "File path does not exist", e, self.name)
+            print(f"Sorry you entered a false file path")
+        except FileNotFoundError as e:
+            self.handle_log_file("ERROR", "File path or file not found", e, self.name)
+            print(f"Sorry you entered a false file path")
+        except KeyboardInterrupt as e:
+            self.handle_log_file("WARN", "Keyboard interrupted script execution", e, self.name)
+            print(f"Sorry program was interrupted!")
+        finally:
+            return True
